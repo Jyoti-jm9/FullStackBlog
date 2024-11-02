8b9686ddd450d907596a19f1652a835872d47f8f
Jyoti-jm9 <mondaljyoti@gmail.com>
2024-11-03 02:20:03 +0530

    fixing bugs

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,6 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     render={({field: {onChange}}) => (
         <Editor
         initialValue={defaultValue}
+        apiKey='mf2apouq9drt2ywb7msgp13iv48yu31ljvxvafqlhaa14wk2'
         init={{
             initialValue: defaultValue,
             height: 500,
